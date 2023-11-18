69a2e3dfff31ba867553e1c5fcfd5e3479a4df2b
anamulislamshamim <anamulislamshamim117@gmail.com>
2023-11-18 20:43:53 +0600

    updated

--- app.py
+++ app.py
@@ -1,23 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, validators
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
-# create a flask instance 
-app = Flask(__name__)
-# create a csrf token for validation so that hacker can not perform csrf attack
-app.config['SECRET_KEY'] = "my_secret_key"
+# create database instance 
+db = SQLAlchemy()
+
+
+# create_app function holds all necessary things regarding app and database configuration 
+def create_app():
+    # create a flask app instance 
+    app = Flask(__name__)
+    # configure the sqlite3 database 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+    # to avoid unnecessary errors with database config the followings
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+    # create a csrf token for validation so that hacker can not perform csrf attack
+    app.config['SECRET_KEY'] = "my_secret_key"
+    # initialize the database for app
+    db.init_app(app)
+    # push the app context
+    app.app_context().push()
+    return app
+
+
+app = create_app()
+
+
+# Create Database Model. Note: Model is actually nothing but database table.
+class Users(db.Model):
+    # here specify the fields 
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Name %r>" % self.name 
 
 
 # create a form class 
-class NamerForm(FlaskForm):
-    name = StringField("What's your Name", validators=[DataRequired()])
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired(), validators.Email("Please enter your email address.")])
+    email = StringField("Email", validators=[DataRequired()])
     submit = SubmitField("Submit")
     
 
 # create necessary routes
-
 # localhost:5000
 @app.route("/")
 def index():
@@ -49,11 +81,29 @@ def page_not_found(e):
 @app.route('/name', methods=['GET', 'POST'])
 def name():
     name = None 
-    form = NamerForm()
+    form = UserForm()
     if form.validate_on_submit():
         name = form.name.data 
         form.name.data = ''
+        flash(f"{name} added successfully!", "info")
         
     return render_template('name.html',
                            name=name,
                            form=form)
+
+
+# create route /user/add for GET and POST methods
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None 
+    form = UserForm()
+    if form.validate_on_submit():
+        # validate whether this email is already exist in our system. If user exist then return the email otherwise will return None 
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data) 
+            db.session.add(user)
+            db.session.commit()
+            flash(f"{form.name.data} added successfully!")
+        name = form.name.data 
+    return render_template('add_user.html', form=form, name=name)
---
+++ static/js/myfile.js
@@ -0,0 +1,15 @@
+document.getElementById('demo').innerHTML = "This was created with JavaScript!";
+
+/* 
+toast button
+*/
+
+const toast_div = document.getElementById('toast-div');
+const toast_btn = document.getElementById("toast-button");
+
+
+toast_btn.addEventListener('click', () => {
+    toast_div.style = "display: none;";
+})
+
+
